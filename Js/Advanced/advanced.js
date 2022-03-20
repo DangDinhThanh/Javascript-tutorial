3a4f455da6d0e4ae56631a4265261e09514a6972
DangDinhThanh <75031076+DangDinhThanh@users.noreply.github.com>
2022-03-21 01:22:36 +0700

    Add files via upload

---
+++ Js/Advanced/advanced.js
@@ -0,0 +1,18 @@
+/**
+ *  Syntax Errors:Lỗi cú pháp
+ * như ví dụ dưới đây là thiếu dấu ngoặc đơn ở window.print(
+ *  Runtime Errors: lỗi thời gian chạy
+ *  VD: window.printme: cú pháp đúng nhưng tại thời gian chạy nhưng nó gọi một phương thức không tồn tại
+ *
+ *
+ */
+<script type = "text/javascript">
+      {
+      window.print(;
+      window.printme();
+</script>
+
+var a = 100
+alert("Value of variable a is : " + a );
+
+console(a)
